b42090a2b0370ecf22b42ef5466116c5961eb2b4
Ryan James Spencer <spencer.ryanjames@gmail.com>
2018-11-15 14:15:50 +1100

    Bump minor version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Dal.MixProject do
     [
       app: :dal,
       name: "DAL",
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
